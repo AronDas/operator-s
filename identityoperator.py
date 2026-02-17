3737c247a6cdd034b476fc6b86c304075c030632
AronDas <arondas.sg@gmail.com>
2026-02-17 18:17:43 -0400

    Add files via upload

---
+++ identityoperator.py
@@ -0,0 +1,13 @@
+
+a = [1,2,3,4]
+b = [1,2,3,4]
+c = a
+print("the id of a is,", id(a))
+print("the id of b is,", id (b))
+print("the id of c is,", id (c))
+print(a is b)
+print(a is c)
+print(b is c)
+print(a is not b)
+print(b is not c)
+
